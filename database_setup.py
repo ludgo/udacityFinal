f0235a442f86a80c55a5c4292c1014484f9b5d4d
ludgo <ludgogit@gmail.com>
2015-06-25 21:45:58 +0200

    Prepared modified database files

---
+++ database_setup.py
@@ -0,0 +1,47 @@
+# Configuration 1
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+
+#Class
+class Restaurant(Base):
+	#Table
+    __tablename__ = 'restaurant'
+   
+    #Mappers
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))
+    city = Column(String(80))
+    state = Column(String(80))
+ 
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+ 
+# Configuration 2
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
